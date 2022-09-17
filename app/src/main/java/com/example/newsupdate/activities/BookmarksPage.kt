9049451d30c37a020c289b637cc018d8b9a50557
siddhant goyal <siddhantgoyal3364@gmail.com>
2022-09-17 09:41:54 +0530

    Added changes

---
+++ app/src/main/java/com/example/newsupdate/activities/BookmarksPage.kt
@@ -0,0 +1,12 @@
+package com.example.newsupdate.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.newsupdate.R
+
+class BookmarksPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bookmarks_page)
+    }
+}
